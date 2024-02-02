7d41adae9b933e72d6001bd4e2488470029f9732
Alexander Foerster <mail@alexander-foerster.com>
2024-02-02 12:00:10 +0100

    CRUD-Interface für Prüfungen rudimentär implementiert

--- src/main/java/com/alexanderfoerster/views/pruefungen/PruefungenDetailView.java
+++ src/main/java/com/alexanderfoerster/views/pruefungen/PruefungenDetailView.java
@@ -3,14 +3,27 @@ package com.alexanderfoerster.views.pruefungen;
 import com.alexanderfoerster.data.Pruefung;
 import com.alexanderfoerster.services.PruefungService;
 import com.alexanderfoerster.views.MainLayout;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.confirmdialog.ConfirmDialog;
+import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.NotificationVariant;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.BeanValidationBinder;
+import com.vaadin.flow.data.binder.ValidationException;
+import com.vaadin.flow.data.converter.StringToIntegerConverter;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import jakarta.annotation.security.RolesAllowed;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
 
 import java.util.Optional;
 
@@ -21,7 +34,15 @@ public class PruefungenDetailView extends VerticalLayout implements BeforeEnterO
     private PruefungService pruefungService;
     private final static String PRUEFUNG_ID = "pruefungID";
     private Optional<Pruefung> pruefungFromBackend = Optional.empty();
-    private final TextField pruefungsIdTF;
+    private TextField id;
+    private TextField bezeichnung;
+    private TextField anzTeilnehmer;
+    private DatePicker datum;
+
+    private final Button saveButton = new Button("Save changes");
+    private final Button deleteButton = new Button("Delete entry");
+
+    private final BeanValidationBinder<Pruefung> binder;
 
     @Override
     public void beforeEnter(BeforeEnterEvent event) {
@@ -37,9 +58,10 @@ public class PruefungenDetailView extends VerticalLayout implements BeforeEnterO
 
     private void updateView() {
         if(pruefungFromBackend.isPresent()) {
-            pruefungsIdTF.setValue(Long.toString(pruefungFromBackend.get().getId()));
+            id.setValue(Long.toString(pruefungFromBackend.get().getId()));
+            binder.readBean(pruefungFromBackend.get());
         } else {
-            pruefungsIdTF.setValue("n.a.");
+            id.setValue("n.a.");
         }
     }
 
@@ -48,9 +70,75 @@ public class PruefungenDetailView extends VerticalLayout implements BeforeEnterO
 
         add(new H1("Prüfungsdetails"));
 
-        pruefungsIdTF = new TextField("Prüfungs-ID");
-        pruefungsIdTF.setEnabled(false);
-        pruefungsIdTF.setValue("---");
-        add(pruefungsIdTF);
+        FormLayout formLayout = new FormLayout();
+        this.id = new TextField("Prüfungs-ID");
+        this.id.setEnabled(false);
+        this.datum = new DatePicker("Datum");
+        this.bezeichnung = new TextField("Bezeichnung");
+        this.anzTeilnehmer = new TextField("Anzahl Teilnehmer");
+        anzTeilnehmer.setEnabled(false);
+        formLayout.add(id, datum, bezeichnung, anzTeilnehmer);
+
+        add(formLayout);
+
+        binder = new BeanValidationBinder<>(Pruefung.class);
+
+        binder.forField(anzTeilnehmer)
+                        .withConverter(new StringToIntegerConverter("Please enter a number"))
+                                .bind("anzTeilnehmer");
+        //binder.bind(bezeichnung, "bezeichnung");
+        //binder.bind(datum, "datum");
+        binder.bindInstanceFields(this);
+
+        saveButton.addClickListener(event -> {
+            if(pruefungFromBackend.isPresent()) {
+                try {
+                    binder.writeBean(this.pruefungFromBackend.get());
+                    pruefungService.update(this.pruefungFromBackend.get());
+                } catch (ValidationException e) {
+                    Notification n = Notification.show("Validation error");
+                    n.setPosition(Notification.Position.MIDDLE);
+                    n.addThemeVariants(NotificationVariant.LUMO_ERROR);
+                } catch (ObjectOptimisticLockingFailureException exception) {
+                    Notification n = Notification.show(
+                            "Error updating the data. Somebody else has updated the record while you were making changes.");
+                    n.setPosition(Notification.Position.MIDDLE);
+                    n.addThemeVariants(NotificationVariant.LUMO_ERROR);
+                }
+                UI.getCurrent().navigate(PruefungenRealView.class);
+            }
+        });
+
+
+        deleteButton.addClickListener( event -> {
+            ConfirmDialog confirmDialog = new ConfirmDialog();
+            confirmDialog.setHeader("Delete?");
+            confirmDialog.setText("Are you shure to delete this entry?");
+            confirmDialog.setCancelable(true);
+            confirmDialog.setConfirmText("Delete");
+            confirmDialog.setConfirmButtonTheme("error primary");
+            confirmDialog.addCancelListener(cancelEvent -> {
+               confirmDialog.close();
+            });
+            confirmDialog.addConfirmListener(confirmEvent -> {
+                if(pruefungFromBackend.isPresent()) {
+                    try {
+                        pruefungService.delete(this.pruefungFromBackend.get().getId());
+                    } catch (ObjectOptimisticLockingFailureException exception) {
+                        Notification n = Notification.show(
+                                "Error updating the data. Somebody else has updated the record while you were making changes.");
+                        n.setPosition(Notification.Position.MIDDLE);
+                        n.addThemeVariants(NotificationVariant.LUMO_ERROR);
+                    }
+                    UI.getCurrent().navigate(PruefungenRealView.class);
+                }
+            });
+            confirmDialog.open();
+        });
+        deleteButton.addThemeVariants(ButtonVariant.LUMO_ERROR);
+
+        HorizontalLayout h = new HorizontalLayout();
+        h.add(saveButton, deleteButton);
+        add(h);
     }
 }
